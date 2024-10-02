6834ce5b1f41e8b3ba6894626153191e487eb460
Thygasc <coelho.thiago189@gmail.com>
2024-10-01 23:06:34 -0300

    Atividade introdutoria em sala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    { title: 'Navegacao', url:'/navegacao', icon: 'code'},
+    { title: 'Botões', url: '/botao', icon: 'construct'},
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
